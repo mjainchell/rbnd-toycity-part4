2934c59d295923bea323078204dca77f67a62d00
Mark Jainchell <mjainchell@yahoo.com>
2016-08-28 19:54:14 -0400

    errors.rb complete and errors are raised as expected

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,1 +1,11 @@
 # Your custom error classes will be placed here
+class ProductNotFoundErrors
+
+  class NotValidID < StandardError
+    def initialize(message = 'Not A Valid ID!')
+      super
+    end
+  end
+
+end
+
